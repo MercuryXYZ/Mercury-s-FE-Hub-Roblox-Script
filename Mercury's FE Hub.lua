d19bac498956e0501297621dbca695d6ffe2697e
MercuryXYZ <89616128+MercuryXYZ@users.noreply.github.com>
2023-01-16 18:43:17 +0100

    Update Mercury's FE Hub.lua

--- Mercury's FE Hub.lua
+++ Mercury's FE Hub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(“https://raw.githubusercontent.com/MercuryXYZ/Mercury-s-FE-Hub-Roblox-Script/main/source.lua”, true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/MercuryXYZ/Mercury-s-FE-Hub-Roblox-Script/main/source.lua', true))()
