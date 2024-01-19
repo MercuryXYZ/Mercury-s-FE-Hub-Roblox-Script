507589eb2b9e5fa2136709ef0134b05f885e5bd8
MercuryXYZ <89616128+MercuryXYZ@users.noreply.github.com>
2024-01-19 21:01:59 +0100

    Update Mercury's FE Hub.lua

--- Mercury's FE Hub.lua
+++ Mercury's FE Hub.lua
@@ -1,1 +1,3 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/MercuryXYZ/Mercury-s-FE-Hub-Roblox-Script/main/source.lua', true))()
+
+--This script is no longer being developed because it is already 2 years old
